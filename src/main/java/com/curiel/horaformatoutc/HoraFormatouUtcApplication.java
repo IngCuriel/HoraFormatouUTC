f4cbcbfcd41fb3b4565189d98cd692693bccdf18
eleazar.curiel <eleazar.curiel@famsa.com>
2021-05-02 17:43:19 -0500

    se crea el proyecto con spring Initializr

---
+++ src/main/java/com/curiel/horaformatoutc/HoraFormatouUtcApplication.java
@@ -0,0 +1,13 @@
+package com.curiel.horaformatoutc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraFormatouUtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraFormatouUtcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curiel/horaformatoutc/HoraFormatouUtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curiel.horaformatoutc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraFormatouUtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
